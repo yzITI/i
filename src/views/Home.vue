84d14a0686556c2e56482a352d0611e084242289
Phantomlsh <phantomlsh@outlook.com>
2021-12-08 10:26:25 -0800

    FIx build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="min-h-screen bg-gray-700 flex flex-col items-center justify-center text-white">
-    <img src="ITI.svg">
+    <img src="/ITI.svg">
     <p class="m-2">A Web Team</p>
     <a href="https://dev.yzzx.org/ITI/">Learn more about us</a>
   </div>
